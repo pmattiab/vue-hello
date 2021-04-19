60d0a57df1b3c30735743d910be2ca9df34916d1
Mattia Bocchino <pmattiab@live.it>
2021-04-19 15:37:45 +0200

    completato

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var app = new Vue(
+    {
+      el: "#root",
+      data: {
+          h1_text: "Hello!",
+          h2_text: "Benvenuto in",
+          h1_img: "https://i.giphy.com/media/LY8yDak6Tngb6FfPrt/giphy.webp",
+          h2_img: "https://thumbs.gfycat.com/PinkPiercingBull-size_restricted.gif"
+      }
+    }
+ );
